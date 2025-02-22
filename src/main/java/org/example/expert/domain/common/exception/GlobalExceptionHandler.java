e6c2bdd8e342d474c72a4a09b9a9cb764f3a3676
seungminJ <seungmin103@gmail.com>
2025-02-22 21:27:33 +0900

    레벨 1-3: Validation - GlobalExceptionHandler 추가

---
+++ src/main/java/org/example/expert/domain/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package org.example.expert.domain.common.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleConstraintViolation(ConstraintViolationException ex) {
+        String errorMessage = ex.getConstraintViolations()
+                .stream()
+                .map(violation -> violation.getMessage())
+                .collect(Collectors.joining(", "));
+        throw new InvalidRequestException(errorMessage);
+    }
+
+    @ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<String> handleInvalidRequest(InvalidRequestException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
